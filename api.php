85b70dc2fd797cb2e8bb70b14812fbb70efa593e
CipherX-Nox <157412343+CipherX-Nox@users.noreply.github.com>
2024-01-30 01:00:05 +0400

    Update api.php

--- api.php
+++ api.php
@@ -3,27 +3,17 @@
 require 'rubika.php';
 
 
-$auth = $_SERVER['HTTP_AUTH'];
-$key = $_SERVER['HTTP_KEY'];
-$method = $_SERVER['HTTP_METHOD'];
+$auth = '';
+$key = '';
 
 $privateKey = <<<EOD
 -----BEGIN RSA PRIVATE KEY-----
-$private
+$key
 -----END RSA PRIVATE KEY-----
 EOD;
 
 
-$bot = new rubika($auth, $private_key);
+$bot = new rubika($auth, $privateKey);
 
-$inputData = file_get_contents("php://input");
-$jsonData = json_decode($inputData, true);
-
-if ($method === 'getChats') {
-    echo $bot->getChats();
-}
-else {
-    echo "Invalid method";
-}
-
+echo $bot->getChats();
 ?>
